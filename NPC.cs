34577622275c6e5ed7c5e9dd2eaf57a4cb36a84b
romchikpromchik123 <rn.ivanov90@gmail.com>
2025-08-06 19:18:08 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровье NPC
+    public int health = 5;
+
+    //Уровень NPC
+    public int level = 1;
+
+    //Скорость NPC
+    public float speed = 1.5f;
+
+    //Положение NPC
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = health + level;
+        print("хп:" + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
